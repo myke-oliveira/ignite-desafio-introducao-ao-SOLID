5c4be462f5b8a9028309b0a147533de254a20050
Myke Albuquerque Pinto de Oliveira <mykeapo@gmail.com>
2021-03-29 16:10:25 -0300

    ✨ NEW FEATURE: List all users route

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -9,7 +9,15 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const userAcessing = this.usersRepository.findById(user_id);
+
+    if (!userAcessing.admin) {
+      throw new Error("Not Allowed");
+    }
+
+    const users = this.usersRepository.list();
+
+    return users;
   }
 }
 
